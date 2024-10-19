36d55b952e0f4b0d68ebbbadc2ea2ff80b49732d
Brian Novak <bnovak1@users.noreply.github.com>
2024-10-19 10:38:12 -0500

    Run HDBSCAN clustering on collective variables data for various values of min_samples and save the number of clusters to a file

---
+++ scripts/clustering.py
@@ -0,0 +1,49 @@
+"""
+Run HDBSCAN clustering on collective variables data for various values of min_samples and save the number of clusters to a file.
+"""
+
+import argparse
+from pathlib import Path
+import sys
+
+import joblib
+import numpy as np
+from sklearn.cluster import HDBSCAN
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+sys.path.append(Path("../scripts"))
+from read_colvar_file import read_colvar
+
+# Command line inputs
+parser = argparse.ArgumentParser()
+parser.add_argument("--lignol", type=str, help="Lignol abbreviation")
+parser.add_argument("--colvar", type=str, help="Input file with collective variables data")
+parser.add_argument("--min_samples", type=int, help="Minimum number of samples")
+parser.add_argument("--model_file", type=str, default=None,
+                    help="File name to save the clustering model to")
+parser.add_argument("--nclusters_file", type=str, default=None,
+                    help="File name to save the number of clusters to")
+args = parser.parse_args()
+
+# Read collective variables
+colvars = read_colvar(args.colvar, args.lignol)
+
+# Scale and cluster the data
+X = colvars.copy().values
+pipe = Pipeline(
+    [
+        ("scaler", StandardScaler()),
+        ("clustering", HDBSCAN(min_cluster_size=args.min_samples, n_jobs=-1)),
+    ]
+)
+model = pipe.fit(X)
+
+# Save the model
+if args.model_file:
+    joblib.dump(model, args.model_file)
+
+# Save the number of clusters
+if args.nclusters_file:
+    nclusters = model["clustering"].labels_.max() + 1
+    np.savetxt(args.nclusters_file, [nclusters], fmt="%d")
