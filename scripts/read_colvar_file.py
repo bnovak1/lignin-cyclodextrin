2379c8c743eaaa579b6f2f2bec3233286c45ccfc
Brian Novak <bnovak1@users.noreply.github.com>
2024-11-05 16:39:16 -0600

    Specify collective variables to extract from colvar file

--- scripts/read_colvar_file.py
+++ scripts/read_colvar_file.py
@@ -1,24 +1,30 @@
+import numpy as np
 import pandas as pd
 
 
-def read_colvar(colvar_file):
+def read_colvar(colvar_file, cv_list=["dnorm", "dtang", "orient"]):
     """
-    Read the colvar file and extract the collective variables; 'dnorm', 'dtang', and 'orient'
+    Read the colvar file and extract the specified collective variables in cv_list.
 
     Parameters
     ----------
     colvar_file : str
         Path to the colvar file.
+    cv_list : list of str, default=["dnorm", "dtang", "orient"]
+        List of collective variables to extract from the colvar file.
 
     Returns
     -------
     pd.DataFrame
-        A DataFrame containing the columns 'dnorm', 'dtang', and 'orient' from the colvar file.
+        A DataFrame containing the columns in cv_list from the colvar file.
     """
 
-    # Read the colvar file
-    colvars = pd.read_csv(
-        colvar_file, sep="\s+", header=None, skiprows=1, names=["dnorm", "dtang", "orient"]
-    )
+    # Read the colvar file header to get the column names
+    with open(colvar_file, "r", encoding="utf-8") as f:
+        cols = f.readline().split()[2:]
 
-    return colvars
+    # Read the colvar file and save the data to a DataFrame
+    colvars = pd.DataFrame(np.loadtxt(colvar_file, skiprows=1), columns=cols)
+
+    # Return the requested collective variables
+    return colvars[cv_list]
