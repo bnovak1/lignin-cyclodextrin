d10f74cc3e71391450439ec4e6be4d078786dac3
Brian Novak <bnovak1@users.noreply.github.com>
2025-01-26 15:09:28 -0700

    Updated Quarto document

--- scripts/render_qmd.sh
+++ scripts/render_qmd.sh
@@ -7,9 +7,8 @@ doc_type=$2
 # Get the qmd file name without the extension
 qmd_name=${qmd_file%.*}
 
-# Use jinja to create an intermediate qmd file
+# Use jinja2 to create an intermediate qmd file
 python ../../scripts/jinja2_apply.py $qmd_file $doc_type
-# jinja $qmd_file -D output_format $doc_type -o $doc_type.qmd
 
 # Render the intermediate qmd file to the desired format
 quarto render $doc_type.qmd --to $doc_type -o ${qmd_name}.${doc_type}
